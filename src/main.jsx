884d777008a2ae4ceab085a5a7be4c66b68ee34d
ZBrian99 <brianzelada1999@gmail.com>
2024-06-01 13:40:57 -0300

    fix bg

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 import './index.scss'
-import { Test } from './Test'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
